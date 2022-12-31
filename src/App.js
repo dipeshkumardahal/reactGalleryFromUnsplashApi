3a26cd7f2f210d3a8f3b1612d2336b111018ae25
dipeshkumardahal <dipeshkumardahal@outlook.com>
2022-12-31 19:40:13 +0545

    search images

--- src/App.js
+++ src/App.js
@@ -5,11 +5,31 @@ import { ACCESS_KEY } from './config/constants';
 
 function App() {
   const [imageList, setImageList] = useState([]);
+  const [tempImage, setTempImage] = useState([]);
   useEffect(() => {
     axios.get(`https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`)
-    .then((response)=>setImageList(response.data));
+    .then((response)=>{
+      setImageList(response.data);
+      setTempImage(response.data);
+    });
   }, []);
+  const searchImage = (search)=>{
+    if(search===""){
+      setImageList(tempImage);
+    }
+    else{
+      const searchedImageList = imageList.filter((data)=>{
+        data.alt_description = data.alt_description == null ? "No":data.alt_description;
+        return data.alt_description.includes(search);
+      });
+      setImageList(searchedImageList);
+    }
+  }
   return (
+    <div>
+      <center>
+      <input type="text" placeholder="Search Your Image" onChange={(e)=>{searchImage(e.target.value)}} />
+      </center>
     <div className="App">
       {imageList.map((data)=>{
         return <div key={data.id} className="images">
@@ -18,6 +38,7 @@ function App() {
           <i>{data.alt_description?data.alt_description.substring(0, 30):"No Caption"}</i>
           </div>
       })}
+    </div>
     </div>
   );
 }
